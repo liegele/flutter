494231da7da41dba305879c344ca6604ff29c809
liegele <liegele@gmail.com>
2022-07-07 22:45:07 -0300

    Adicionando widgets no app

--- the_complete_2021_flutter_development_bootcamp_with_dart/i_am_rich/lib/main.dart
+++ the_complete_2021_flutter_development_bootcamp_with_dart/i_am_rich/lib/main.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 //The main function is the starting point for all Flutter apps.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
+    MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueGrey[900],
+          body: const Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://s2.glbimg.com/7NzvJnGtmLTc1d5TRf8YsAbTuz8=/e.glbimg.com/og/ed/f/original/2016/09/27/aurora_boreal.jpg'),
+            ),
+          )),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
